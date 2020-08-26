1b723599164783fcbf675f803ffcda63b6f75fa8
Alexander Komarov <evilantik@gmail.com>
2020-08-26 20:20:29 +0300

    init commit 6

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,4 +6,20 @@ firebase.initializeApp({
     messagingSenderId: '8780804191'
 });
 
+/*
+Retrieve an instance of Firebase Messaging so that it can handle background messages.
+*/
 const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notification = JSON.parse(payload.data.notification);
+    // Customize notification here
+    const notificationTitle = notification.title;
+    const notificationOptions = {
+        body: notification.body,
+        icon: notification.icon
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
